018061ac16024dc3252af90e15dc23368460c670
James Whinfrey <james@conceptric.co.uk>
2011-04-15 13:12:56 +0100

    create initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capitalist::Application.config.secret_token = 'd36f38a733ac8540abac745a3ba1a181e4ef1f045630e68287d3bb73c4fb17143960764167ca87325ec3174a64b1391231014296404516ade3d303f582b99be4'

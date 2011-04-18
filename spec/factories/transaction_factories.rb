48c594877a684147eb45fabbd09829b681f9fda7
James Whinfrey <james@conceptric.co.uk>
2011-04-18 17:24:36 +0100

    Added total_value and cost parameters to my factory to ensure that they could take decimals.

--- spec/factories/transaction_factories.rb
+++ spec/factories/transaction_factories.rb
@@ -1,4 +1,6 @@
 Factory.define :transaction, :class => Transaction do |t|
   t.date Date.new(2010,1,1)
-  t.association :asset, :factory => :asset
+  t.association :asset, :factory => :asset     
+  t.total_value 100.10
+  t.cost 10.01
 end

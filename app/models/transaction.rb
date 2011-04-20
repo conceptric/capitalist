535077aa0da62b1fe824c49117d888aa98ba7b0d
James Whinfrey <james@conceptric.co.uk>
2011-04-20 17:58:47 +0100

    Added the units attribute to the transaction views.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ActiveRecord::Base
   belongs_to :asset
-  attr_accessible :date, :asset_id, :total_value, :cost
+  attr_accessible :date, :asset_id, :units, :total_value, :cost
   
   validates_presence_of :date, :asset_id, :units
   validates_numericality_of :units, :greater_than_or_equal_to => 0.00001

77805fc27639d8d956d0ca07088031a24db4c367
James Whinfrey <james@conceptric.co.uk>
2011-04-20 16:40:50 +0100

    Refactored the asset controller specs to match those for the transactions, they read more clearly.

--- spec/controllers/assets_controller_spec.rb
+++ spec/controllers/assets_controller_spec.rb
@@ -3,25 +3,29 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe AssetsController do
   render_views
 
-  it "index action should render index template" do
-    get :index
-    response.should render_template(:index)
+  describe "Creating a new asset" do
+    it "new action should render new template" do
+      get :new
+      response.should render_template(:new)
+    end
+
+    it "create action should render new template when model is invalid" do
+      Asset.any_instance.stubs(:valid?).returns(false)
+      post :create
+      response.should render_template(:new)
+    end
+
+    it "create action should redirect when model is valid" do
+      Asset.any_instance.stubs(:valid?).returns(true)
+      post :create
+      response.should redirect_to(assets_url)
+    end
   end
 
-  it "new action should render new template" do
-    get :new
-    response.should render_template(:new)
-  end
-
-  it "create action should render new template when model is invalid" do
-    Asset.any_instance.stubs(:valid?).returns(false)
-    post :create
-    response.should render_template(:new)
-  end
-
-  it "create action should redirect when model is valid" do
-    Asset.any_instance.stubs(:valid?).returns(true)
-    post :create
-    response.should redirect_to(assets_url)
+  describe "Showing existing assets" do
+    it "index action should render index template" do
+      get :index
+      response.should render_template(:index)
+    end
   end
 end

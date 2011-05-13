37fd1bb956f6fedaafc450d0642a228735e408c7
James Whinfrey <james@conceptric.co.uk>
2011-05-13 13:55:41 +0100

    Removed all reference to transactions in the asset model and specs.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,9 +1,6 @@
 class Asset < ActiveRecord::Base
   attr_accessible :name, :description
   has_many :positions
-  has_many :transactions, :order => "date ASC"
-  has_many :purchases, :order => "date ASC"
-  has_many :sales, :order => "date ASC"
   
   validates :name, :presence => true, 
             :uniqueness => true,

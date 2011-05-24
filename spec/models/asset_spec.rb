4bcae4884898ce935c1745dd59c5137023bd5e5f
James Whinfrey <james@conceptric.co.uk>
2011-05-24 16:45:22 +0100

    Cleaned up the spec descriptions for Asset, no change in functionality.

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -10,37 +10,37 @@ describe Asset, ".new" do
   end
 
   describe "Name attribute" do
-    it "should be valid with a name" do
+    it "is a string" do
       @asset.should be_valid
     end
 
-    it "should have a unique name" do
+    it "is unique" do
       Factory.build(:asset, @asset.attributes).should_not be_valid
     end
 
-    it "should be invalid without a name" do
+    it "cannot be nil" do
       @asset.name = nil
       @asset.should_not be_valid
     end
 
-    it "should be invalid if the name is longer than 20 characters" do
+    it "cannot be longer than 20 characters" do
       @asset.name = 'abcdeabcdeabcdeabcdef'
       @asset.should_not be_valid
     end    
   end
 
   describe "Positions attribute" do
-    it "should be allowed to be empty" do
+    it "is allowed to be empty" do
       @asset.positions.should be_empty
     end                        
     
-    it "should be associated with Positions" do
+    it "is associated with Positions" do
       asset = Factory(:asset_with_position)
       asset.positions.size.should eql(1)
       asset.positions.first.should be_instance_of(Position)
     end
 
-    it "should cascade delete associated Positions" do
+    it "cascades deletion to associated Positions" do
       asset = Factory(:asset_with_position)
       Position.all.size.should eql(1)
       asset.destroy                 
@@ -56,16 +56,16 @@ describe Asset, ".current_units" do
     @asset = Factory(:asset)
   end
 
-  it "should be zero when there are no positions" do
+  it "is zero when there are no positions" do
     @asset.current_units.should eql(0)    
   end
 
-  it "should be total units held a single position" do    
+  it "is the units held in a single position" do    
     Factory(:position, :asset => @asset)
     @asset.current_units.should eql(5)
   end
 
-  it "should be the sum of total units in two positions" do
+  it "is the sum of the units held in two positions" do
     Factory(:position, :asset => @asset)
     Factory(:position, :asset => @asset)
     @asset.current_units.should eql(10)

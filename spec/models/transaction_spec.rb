0b1d062fdaf230b48fab51f31bc54fce228a10d5
James Whinfrey <james@conceptric.co.uk>
2011-05-24 16:26:28 +0100

    Refactored duplicate code from the transaction specs into a shared example.

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,114 +1,104 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 shared_examples_for "A Transaction" do
-  it "should be valid" do
+  it "is valid" do
     @transaction.should be_valid    
   end
-  
+
   describe "Date attribute" do
-    it "should have a date" do
+    it "has a date" do
       @transaction.date = nil
       @transaction.should_not be_valid
     end
-  end
 
+    it "is a valid date" do
+      @transaction.date.should be_instance_of(Date)
+      @transaction.date.should eql(@date)
+    end    
+  end
+  
   describe "Position attribute" do
-    it "should be associated to a Position" do
+    it "is associated to a Position" do
       @transaction.position.should be_instance_of(Position)
     end
 
-    it "should not allow the associated Position to be nil" do
+    it "can be nil" do
       @transaction.position = nil
       @transaction.should_not be_valid
     end                                                            
   end
 
   describe "Units attribute" do
-    it "should comprise a decimal number of units" do 
+    it "is a decimal number of units" do 
       @transaction.units = 1111111.11111
       @transaction.units.should eql(1111111.11111)
     end
 
-    it "should not allow the number of units to be blank" do
+    it "can be blank" do
       @transaction.units = nil
       @transaction.should_not be_valid
     end                               
 
-    it "should allow the number of units to be a small decimal" do
+    it "allows a small decimal" do
       @transaction.units = 0.00001
       @transaction.should be_valid
     end                               
 
-    it "should not allow units to use more than 5 decimal places" do
+    it "is no more than 5 decimal places" do
       @transaction.units = 0.000009
       @transaction.should_not be_valid
     end                               
     
-    it "should not allow the number of units to be zero" do
+    it "cannot be zero" do
       @transaction.units = 0
       @transaction.should_not be_valid
     end                               
 
-    it "should not allow the number of units to be negative" do
+    it "cannot be negative" do
       @transaction.units = -0.00001
       @transaction.should_not be_valid
     end                                   
   end
-
+  
+  describe "Value attribute" do
+    it "has the correct decimal format" do
+      @transaction.value.should eql(@value)
+    end  
+  end
+  
+  describe "Expenses attribute" do    
+    it "has the correct decimal format" do
+      @transaction.expenses.should eql(@expense)
+    end      
+  end                      
 end
 
 describe Purchase, ".new" do
   before :each do
     @transaction = Factory.build(:purchase)
+    @value = 100.10
+    @expense = 10.01
+    @date = Date.new(2010,1,1)
   end
   
   it_should_behave_like "A Transaction"
 
-  describe "Decimal attributes" do
-    it "should return the correct value decimal" do
-      @transaction.value.should eql(100.10)
-    end  
-
-    it "should return the correct cost decimal" do
-      @transaction.expenses.should eql(10.01)
-    end      
-  end                      
-
-  describe "Date attribute" do
-    it "should have a valid date" do
-      @transaction.date.should be_instance_of(Date)
-      @transaction.date.should eql(Date.new(2010,1,1))
-    end    
-  end
 end
 
 describe Sale, ".new" do
   before :each do
     @position = Factory(:open_position)
     @transaction = Factory.build(:sale, :position => @position)
+    @value = 200.10
+    @expense = 10.01
+    @date = Date.new(2011,1,1)
   end
   
   it_should_behave_like "A Transaction"
 
-  describe "Decimal attributes" do
-    it "should return the correct value decimal" do
-      @transaction.value.should eql(200.10)
-    end  
-
-    it "should return the correct cost decimal" do
-      @transaction.expenses.should eql(10.01)
-    end      
-  end                      
-
-  describe "Date attribute" do
-    it "should have a valid date" do
-      @transaction.date.should be_instance_of(Date)
-      @transaction.date.should eql(Date.new(2011,1,1))
-    end    
-  end
 end
 
-describe Sale, " validates there is something to sell" do
+describe Sale, " validation" do
   before :each do
     @position = Factory(:open_position)
     @transaction = Factory.build(:sale, :position => @position)
@@ -119,46 +109,46 @@ describe Sale, " validates there is something to sell" do
     @transaction.should_not be_valid
   end
   
-  it "is invalid if the sale is before the purchase" do
+  it "prevents selling before buying" do
     @transaction.date = Date.new(2009,1,1)
     @transaction.should_not be_valid
   end
 end
 
-describe Sale, " validates there is enough to sell" do    
+describe Sale, " validates the amount being sold" do    
   before(:each) do
     @position = Factory(:open_position)           
     @transaction = Factory(:sale, :position => @position, :units => 3)
   end
 
-  it "is invalid if selling more units than have been purchased" do
+  it "if selling more units than have been purchased" do
     @transaction.units = 10
     @transaction.should_not be_valid
   end
 
-  it "is invalid if too few units were purchased before the sale" do
+  it "if too few units were purchased before the sale" do
     Factory(:purchase, :position => @position, :date => Date.new(2011,2,1))
     @transaction.units = 10
     @transaction.should_not be_valid
   end
     
-  it "prevents position invalidation when a sale is inserted" do
+  it "once a new sale is inserted before an old one" do
     transaction = Factory.build(:sale, :position => @position)
     transaction.date = Date.new(2010,10,1)
     transaction.should_not be_valid      
   end
 
-  it "is valid when updating an existing sale with fewer units" do
+  it "when reducing the size of an existing sale" do
     @transaction.units = 1     
     @transaction.should be_valid            
   end
 
-  it "is invalid when updating a sale with too many units" do
+  it "when increasing the size of an existing sale too much" do
     @transaction.units = 6     
     @transaction.should_not be_valid            
   end
 
-  it "when updating an existing sale with more available units" do
+  it "when increasing the size of an existing sale to sell everything" do
     @transaction.units = 5
     @transaction.should be_valid            
   end
